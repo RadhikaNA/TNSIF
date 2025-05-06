d73c1e172af384492c5198eb0dd35907d91cc999
Radhika Narayan Appinabail <107504872+RadhikaNA@users.noreply.github.com>
2025-05-06 11:51:37 +0530

    Delete src directory

--- src/main/java/service/admin/mall/MallAdminRepository.java
+++
@@ -1,7 +0,0 @@
-package service.admin.mall;
-import org.springframework.data.jpa.repository.JpaRepository;  
-
-
-public interface MallAdminRepository extends JpaRepository<MallAdmin, Integer>{
-
-}
